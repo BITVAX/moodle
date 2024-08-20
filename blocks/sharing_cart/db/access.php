1f92682bdf333acebd1f539716a36d6d034c9804
Ignacio J. Ortega <nacho.ortega@gmail.com>
2024-08-20 15:49:03 +0200

    [ADD] block_completion_progress, block_sharing_cart, qtype_multichoiceset

---
+++ blocks/sharing_cart/db/access.php
@@ -0,0 +1,17 @@
+<?php
+
+// @codeCoverageIgnoreStart
+defined('MOODLE_INTERNAL') || die();
+// @codeCoverageIgnoreEnd
+
+$capabilities = [
+    'block/sharing_cart:addinstance' => [
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_BLOCK,
+        'archetypes' => [
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW,
+        ],
+        'clonepermissionsfrom' => 'moodle/site:manageblocks',
+    ],
+];
